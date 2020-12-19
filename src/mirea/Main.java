e39dd3f0a0369afbbc2c456629618469a9fc93c9
yakol <ya.kolysh@gmail.com>
2020-12-19 21:32:28 +0300

    Initial commit

---
+++ src/mirea/Main.java
@@ -0,0 +1,26 @@
+package mirea;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Student> arrayList = new ArrayList<>(20);
+
+
+        for (int i = 0; i < 5; i++){
+            arrayList.add(new Student("Test" + i, (int)(Math.random() * 10 + 20)));
+        }
+        System.out.println("Неотсортированный список студентов: ");
+        for (int i = 0; i < 5; i++){
+            System.out.println(arrayList.get(i).getName() + " " + arrayList.get(i).getSumOrder());
+        }
+
+        QuickSort qs = new QuickSort();
+        qs.quickSort(arrayList, 0, arrayList.size() - 1);
+
+        System.out.println("Отсортированный список студентов: ");
+        for (int i = 0; i < 5; i++){
+            System.out.println(arrayList.get(i).getName() + " " + arrayList.get(i).getSumOrder());
+        }
+    }
+}
---
+++ src/mirea/StudentComparator.java
@@ -0,0 +1,9 @@
+package mirea;
+
+import java.util.Comparator;
+
+public class StudentComparator implements Comparator<Student> {
+    public int compare(Student o1, Student o2) {
+        return o1.getSumOrder() - o2.getSumOrder();
+    }
+}
